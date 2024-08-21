14ee94f02e0a4788dd8f5f550b03b6329d2d478a
gudertesteur <gudertesteur@gmail.com>
2024-08-21 11:06:15 +0200

    second commit

--- DigiHello/src/main/java/com/digi/digihello/DigiVillesApplication.java
+++ DigiHello/src/main/java/com/digi/digihello/DigiVillesApplication.java
@@ -3,6 +3,7 @@ package com.digi.digihello;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class DigiVillesApplication {
 
