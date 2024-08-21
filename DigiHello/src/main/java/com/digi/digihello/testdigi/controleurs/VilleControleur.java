8f963bf0eebabd9fdb6b7fe69a2469933c8416ea
gudertesteur <gudertesteur@gmail.com>
2024-08-21 12:14:53 +0200

    TP 4 - Revise

--- DigiHello/src/main/java/com/digi/digihello/testdigi/controleurs/VilleControleur.java
+++ DigiHello/src/main/java/com/digi/digihello/testdigi/controleurs/VilleControleur.java
@@ -2,7 +2,7 @@ package com.digi.digihello.testdigi.controleurs;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,12 +32,12 @@ public class VilleControleur {
     }
 
     // Méthode pour ajouter une nouvelle ville
-    @PostMapping("/ajouter")
+    @PutMapping("/ajouter")
     public ResponseEntity<String> ajouterVille(@RequestBody Ville nouvelleVille) {
         // Vérification si une ville avec le même nom existe déjà
         for (Ville ville : villes) {
             if (ville.getNom().equalsIgnoreCase(nouvelleVille.getNom())) {
-                return new ResponseEntity<>("La ville existe déjà", HttpStatus.CONFLICT);
+                return new ResponseEntity<>("La ville existe déjà", HttpStatus.BAD_REQUEST);
             }
         }
 
